9a53cf0c4cae76b4e6fafac855248e847a47c256
Hemant Verma <vermahemant837@gmail.com>
2026-08-05 16:53:16 +0530

    new

--- frontend/src/services/taskService.js
+++ frontend/src/services/taskService.js
@@ -1,5 +1,6 @@
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
+console.log(API_BASE_URL);
 class TaskService {
   async getAllTasks() {
     const response = await fetch(`${API_BASE_URL}/tasks`);
